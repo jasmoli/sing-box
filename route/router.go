9aa7ea6e69ccc47197eb9e6ff1623ce296f1c19a
yaotthaha <yaotthaha@foxmail.com>
2023-10-10 10:40:10 +0800

    Add reload support
    (cherry picked from commit https://github.com/rnetx/sing-box/commit/e98000c371095c1d5f7fcec4a02ffaaf22a68e47)

--- route/router.go
+++ route/router.go
@@ -103,6 +103,7 @@ type Router struct {
 	needPackageManager                 bool
 	wifiState                          adapter.WIFIState
 	started                            bool
+	reloadChan                         chan<- struct{}
 }
 
 func NewRouter(
@@ -113,6 +114,7 @@ func NewRouter(
 	ntpOptions option.NTPOptions,
 	inbounds []option.Inbound,
 	platformInterface platform.Interface,
+	reloadChan chan<- struct{},
 ) (*Router, error) {
 	router := &Router{
 		ctx:                   ctx,
@@ -143,6 +145,7 @@ func NewRouter(
 		needPackageManager: C.IsAndroid && platformInterface == nil && common.Any(inbounds, func(inbound option.Inbound) bool {
 			return len(inbound.TunOptions.IncludePackage) > 0 || len(inbound.TunOptions.ExcludePackage) > 0
 		}),
+		reloadChan: reloadChan,
 	}
 	router.dnsClient = dns.NewClient(dns.ClientOptions{
 		DisableCache:     dnsOptions.DNSClientOptions.DisableCache,
@@ -1368,5 +1371,14 @@ func (r *Router) notifyWindowsPowerEvent(event int) {
 	case winpowrprof.EVENT_RESUME_AUTOMATIC:
 		r.pauseManager.DeviceWake()
 		_ = r.ResetNetwork()
+	}
+}
+
+func (r *Router) Reload() {
+	if r.platformInterface == nil {
+		select {
+		case r.reloadChan <- struct{}{}:
+		default:
+		}
 	}
 }

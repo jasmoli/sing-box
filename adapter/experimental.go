a66a9bc2d909624dcf6f9fcb05c26e63e0917248
PuerNya <safarier@outlook.com>
2024-01-21 15:18:01 +0800

    Add Outbound Providers

--- adapter/experimental.go
+++ adapter/experimental.go
@@ -105,6 +105,8 @@ type OutboundGroup interface {
 	Outbound
 	Now() string
 	All() []string
+	UpdateOutbounds(tag string) error
+	SelectedOutbound(network string) Outbound
 }
 
 type URLTestGroup interface {

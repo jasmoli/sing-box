bf986d63e9f47b7a52995c6df39c1ad05b0620d1
PuerNya <safarier@outlook.com>
2024-02-05 02:28:52 +0800

    Add multi-server support for dns rule

--- adapter/router.go
+++ adapter/router.go
@@ -6,8 +6,8 @@ import (
 	"net/netip"
 
 	"github.com/sagernet/sing-box/common/geoip"
-	"github.com/sagernet/sing-dns"
-	"github.com/sagernet/sing-tun"
+	dns "github.com/sagernet/sing-dns"
+	tun "github.com/sagernet/sing-tun"
 	"github.com/sagernet/sing/common/control"
 	N "github.com/sagernet/sing/common/network"
 	"github.com/sagernet/sing/service"
@@ -23,6 +23,8 @@ type Router interface {
 	Outbounds() []Outbound
 	Outbound(tag string) (Outbound, bool)
 	DefaultOutbound(network string) (Outbound, error)
+
+	Transport(tag string) (dns.Transport, bool)
 
 	FakeIPStore() FakeIPStore
 
@@ -89,6 +91,7 @@ type DNSRule interface {
 	ClientSubnet() *netip.Addr
 	WithAddressLimit() bool
 	MatchAddressLimit(metadata *InboundContext) bool
+	Servers() []string
 }
 
 type RuleSet interface {

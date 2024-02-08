28ba74ef4481fd5e051ff8ce39a9d32dcc6883f1
PuerNya <safarier@outlook.com>
2024-02-08 16:27:15 +0800

    Add Outbound Providers

--- adapter/experimental.go
+++ adapter/experimental.go
@@ -109,11 +109,14 @@ type OutboundGroup interface {
 	Outbound
 	Now() string
 	All() []string
+	UpdateOutbounds(tag string) error
+	SelectedOutbound(network string) Outbound
 }
 
 type URLTestGroup interface {
 	OutboundGroup
 	URLTest(ctx context.Context) (map[string]uint16, error)
+	PerformUpdateCheck(tag string, force bool)
 }
 
 func OutboundTag(detour Outbound) string {

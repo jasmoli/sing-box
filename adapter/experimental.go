d410ed5a63693f4f1c2b2335620539d3f8150c6d
xireiki <i@xireiki.com>
2025-10-07 02:54:11 +0800

    Add Outbound Provider
    Co-authored-by: yelnoo <t50500411@163.com>
    Co-authored-by: jebbs <qjebbs@gmail.com>
    Co-authored-by: PuerNya <safarier@outlook.com>

--- adapter/experimental.go
+++ adapter/experimental.go
@@ -6,6 +6,7 @@ import (
 	"encoding/binary"
 	"time"
 
+	"github.com/sagernet/sing-box/common/hash"
 	"github.com/sagernet/sing/common/varbin"
 )
 
@@ -52,9 +53,12 @@ type CacheFile interface {
 	StoreGroupExpand(group string, expand bool) error
 	LoadRuleSet(tag string) *SavedBinary
 	SaveRuleSet(tag string, set *SavedBinary) error
+	LoadSubscription(tag string) *SavedBinary
+	SaveSubscription(tag string, sub *SavedBinary) error
 }
 
 type SavedBinary struct {
+	Hash        hash.HashType
 	Content     []byte
 	LastUpdated time.Time
 	LastEtag    string
@@ -63,6 +67,11 @@ type SavedBinary struct {
 func (s *SavedBinary) MarshalBinary() ([]byte, error) {
 	var buffer bytes.Buffer
 	err := binary.Write(&buffer, binary.BigEndian, uint8(1))
+	if err != nil {
+		return nil, err
+	}
+	hash, _ := s.Hash.MarshalBinary()
+	err = binary.Write(&buffer, binary.BigEndian, hash)
 	if err != nil {
 		return nil, err
 	}
@@ -85,6 +94,15 @@ func (s *SavedBinary) UnmarshalBinary(data []byte) error {
 	reader := bytes.NewReader(data)
 	var version uint8
 	err := binary.Read(reader, binary.BigEndian, &version)
+	if err != nil {
+		return err
+	}
+	var hash [16]byte
+	err = binary.Read(reader, binary.BigEndian, &hash)
+	if err != nil {
+		return err
+	}
+	err = s.Hash.UnmarshalBinary(hash)
 	if err != nil {
 		return err
 	}
